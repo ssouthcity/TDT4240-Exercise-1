eb398aa721ef08cd04bca3951b7e73c08ed35476
Stian Sørby <ssouthcity@gmail.com>
2023-01-19 21:14:35 +0100

    scaffold and begin task 1

---
+++ Task 1/core/src/dev/southcity/helicopter/HelicopterBounceGame.kt
@@ -0,0 +1,64 @@
+package dev.southcity.helicopter
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Rectangle
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.utils.ScreenUtils
+import kotlin.random.Random
+
+class HelicopterBounceGame : ApplicationAdapter() {
+    lateinit var batch: SpriteBatch
+    lateinit var img: Texture
+    lateinit var sprite: Sprite
+    lateinit var position: Vector2
+    lateinit var velocity: Vector2
+
+    override fun create() {
+        batch = SpriteBatch()
+
+        img = Texture("badlogic.jpg")
+
+        sprite = Sprite(img)
+        sprite.setOriginCenter()
+
+        position = Vector2(Gdx.graphics.width / 2f, Gdx.graphics.height / 2f)
+
+        velocity = Vector2(
+            Random.nextFloat() - 0.5f,
+            Random.nextFloat() - 0.5f
+        ).setLength(20f)
+
+        sprite.setCenter(position.x, position.y)
+    }
+
+    override fun render() {
+        ScreenUtils.clear(0f, 0f, 0f, 1f)
+
+        position.add(velocity)
+
+        if (position.x < 0 || position.x + sprite.width > Gdx.graphics.width) {
+            position.x = position.x.coerceIn(0f, Gdx.graphics.width.toFloat())
+            velocity.x = -velocity.x
+        }
+
+        if (position.y < 0 || position.y + sprite.height > Gdx.graphics.height) {
+            position.y = position.y.coerceIn(0f, Gdx.graphics.height.toFloat())
+            velocity.y = -velocity.y
+        }
+
+        sprite.setPosition(position.x, position.y)
+
+        batch.begin()
+        sprite.draw(batch)
+        batch.end()
+    }
+
+    override fun dispose() {
+        batch.dispose()
+        img.dispose()
+    }
+}
